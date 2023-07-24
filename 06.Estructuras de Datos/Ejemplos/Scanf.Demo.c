b7a547ee88c81267744f5d463a695380d8e10842
FAUC0N <jorge.varela@inventse.com>
2023-07-24 15:58:17 -0600

    Borré archivo de prueba Scanf.Demo.c

--- 06.Estructuras de Datos/Ejemplos/Scanf.Demo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num;
-    printf("Por favor, ingrese un numero: ");
-    scanf("%d", &num);
-    printf("El número ingresado es: %d\n", num);
-    return 0;
-}
