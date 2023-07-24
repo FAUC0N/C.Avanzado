01e8913669c0f8b3c29abb8c5ce95bb6f3818378
FAUC0N <jorge.varela@inventse.com>
2023-07-24 13:27:30 -0600

    ejercicio temporal

---
+++ 06.Estructuras de Datos/Ejemplos/Scanf.Demo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Por favor, ingrese un numero: ");
+    scanf("%d", &num);
+    printf("El número ingresado es: %d\n", num);
+    return 0;
+}
