d99d063729ab4850a15ca68289fd5828734b52a1
FAUC0N <jorge.varela@inventse.com>
2023-07-26 12:58:22 -0600

    Actualización del código de ejercicio Implementé algunas mejoras en la estructura de datos para hacer validaciones de los nodos.

--- 06.Estructuras de Datos/Ejemplos/Ejercicio.Tree.c
+++ 06.Estructuras de Datos/Ejemplos/Ejercicio.Tree.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int Found;
 
 struct Node{
     int dato;
     struct Node* left;
     struct Node* right;
+    char route[256];
 };
 
-struct Node* createNode(int value){
+struct Node* createNode(int value, const char* route){
     struct Node* newNode = (struct Node*)calloc(1, sizeof(struct Node));
     newNode->dato = value;
     newNode->left = newNode->right = NULL;
+    strncpy(newNode->route, route, sizeof(newNode->route) - 1);
     return newNode;
 }
 
-struct Node* insertValue(struct Node* node, int value){
-    if(node == NULL) return createNode(value);
+struct Node* insertValue(struct Node* node, int value, char* route){
+    if(node == NULL) return createNode(value, route);
 
     if(value < node->dato){
-        node->left = insertValue(node->left, value);
+        char newRoute[256];
+        snprintf(newRoute, sizeof(newRoute), "%sL", route);
+        node->left = insertValue(node->left, value, newRoute);
     }else{
-        node->right = insertValue(node->right, value);
+        char newRoute[256];
+        snprintf(newRoute, sizeof(newRoute), "%sR", route);
+        node->right = insertValue(node->right, value, newRoute);
     }
 
     return node;
+}
+
+void printInOrder(struct Node* node) {
+    if(node == NULL) return;
+
+    printInOrder(node->left);
+    printf("%d ", node->dato);
+    printInOrder(node->right);
+}
+
+void printPreOrder(struct Node* node) {
+    if(node == NULL) return;
+
+    printf("%d ", node->dato);
+    printPreOrder(node->left);
+    printPreOrder(node->right);
 }
 
 void printPostOrder(struct Node* node) {
@@ -33,6 +58,26 @@ void printPostOrder(struct Node* node) {
     printPostOrder(node->right);
     printf("%d ", node->dato);
 }
+
+void searchAndPrint(struct Node* root, int value) {
+    if(root == NULL) return;
+
+    if(root->dato == value) {
+        Found = 1;
+        printf("Nodo: %d, Ruta: %s\n", root->dato, root->route);
+    }
+
+    searchAndPrint(root->left, value);
+    searchAndPrint(root->right, value);
+}
+
+void searchNode(struct Node* root, int value) {
+    Found = 0;
+    searchAndPrint(root, value);
+    if(!Found)
+        printf("El nodo con el valor %d, no existe en el árbol", value);
+}
+
 
 void freeTree(struct Node* node) {
     if (node == NULL) return;
@@ -46,19 +91,29 @@ int main(void){
     // Crear un árbol vacío
     struct Node* root = NULL;
 
-    root = insertValue(root, 5);
-    insertValue(root, 8);
-    insertValue(root, 7);
-    insertValue(root, 9);
-    insertValue(root, 1);
-    insertValue(root, 6);
-    insertValue(root, 2);
-    insertValue(root, 5);
-    insertValue(root, 6);
+    root = insertValue(root, 5, "Root");
+    insertValue(root, 8, "Root");
+    insertValue(root, 7, "Root");
+    insertValue(root, 9, "Root");
+    insertValue(root, 1, "Root");
+    insertValue(root, 6, "Root");
+    insertValue(root, 2, "Root");
+    insertValue(root, 5, "Root");
+    insertValue(root, 6, "Root");
 
-    // Imprimir el árbol en post-order
+    printf("Impresión In-Order\n");
+    printInOrder(root);
+    printf("\nImpresión Post-Order\n");
+    printPostOrder(root);
+    printf("\nImpresión Pre-Order\n");
     printPostOrder(root);
     printf("\n");
+
+    // agregué algunas porpiedades, tan solo como ejercicio para validar la ruta de nodo
+    searchNode(root, 9);
+    searchNode(root, 6);
+    searchNode(root, 2);
+    searchNode(root, 10);
 
     // Liberar la memoria
     freeTree(root);
